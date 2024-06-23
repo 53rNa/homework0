10ca8368dec9199aa253e12aa5ab59eadc461534
53rNa <53rNa76@gmail.com>
2024-06-23 10:38:22 +0300

    Add files via upload
    Решение к практическому заданию по уроку "Базовые структуры данных"

---
+++ main.py
@@ -0,0 +1,18 @@
+a = 'lenght of word'                                        #Задача «Длина слова»
+print (len (a))
+
+first = 23; second = 8                                      #Задача «Суммы и разности»
+summa = first + second
+diff = first - second
+print (summa, diff)
+
+first = 5; second = 4; third = 10                           #Задача «Среднее арифметическое»
+mean = (first + second + third)//3
+print (mean)
+
+first_string = 'Вторник'; second_string = 'Понедельник'     #Задача «Простые строчки»
+print (second_string + ', ' + first_string)
+
+a = 2; b = 4; c = 8                                         #Задача «Сложная формула».
+f = (a * b) + (a * c)
+print ((f ** 3)//2)
